5f84427b92a256a249c83de454ea704bd9089dfd
Samwel Tochora <samtochora2001@gmail.com>
2025-02-21 22:21:42 +0300

    creating signup controller

--- backend/controllers/UserSignup.js
+++ backend/controllers/UserSignup.js
@@ -1,6 +1,35 @@
+const userModels = require('../models/userModels');
+const bcrypt = require('bcryptjs');
+
+
 async function userSignUpController(req, res){
     try {
-        
+        const {email, password, name} = req.body;
+        if(!email){
+            throw new Error('Please proveide Email')
+        }
+        if(!password){
+            throw new Error('Please proveide password')
+        }
+        if(!name){
+            throw new Error('Please proveide name')
+        }
+       const salt = bcrypt.genSaltSync(10);
+       const hashPassword = bcrypt.hashSync(password, salt)
+
+       if(!hashPassword){
+        throw new error('Something is wrong!')
+       }
+       const payload = {...req.body, password:hashPassword}
+       const userData = new userModels(payload)
+       const saveUser = userData.save();
+       res.status(201).json({
+        data : saveUser,
+        success : true,
+        error : false,
+        message : 'User created successfully!'
+       })
+
     } catch (err) {
         res.json({
             message: err,
@@ -9,3 +38,4 @@ async function userSignUpController(req, res){
         })
     }
 }
+module.exports = userSignUpController
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 
 const router = express.Router();
-
+const userSignUpController = require('')
 module.exports = router
